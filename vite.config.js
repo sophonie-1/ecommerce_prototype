879d1a36b04de8ca94324a07c5b71b58ba62f17b
bukira sophonie <bukirasophonie347@gmail.com>
2025-11-29 07:50:41 -0500

    this is project of ecommerce prototype

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/MARZ_Ecommerce/', // Change to your repo name! (e.g., /your-repo-name/)
+    base: '/ecommerce_prototype/', // Change to your repo name! (e.g., /your-repo-name/)
 })
